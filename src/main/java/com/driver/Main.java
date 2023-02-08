7208ac9f85c6620c40c9c27b849c8c7df321417d
Arin <arin1998@gmail.com>
2023-02-08 12:46:35 +0530

    class A and class B made as inner classes of Main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,19 +1,21 @@
 package com.driver;
-
-class A{
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-    public String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-}
 public class Main {
+    static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    static class B extends A{
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
     public static void main(String[] args) {
         B b = new B();
         b.meth();
+
+        B b1 = new B();
+        b1.meth();
     }
 
 }
